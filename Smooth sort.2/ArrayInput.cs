de30f1ac675eae591081c2f167bbe64289eec7b1
gzlm163 <marusyagzl@gmail.com>
2026-06-15 18:03:20 +0700

    8

---
+++ Smooth sort.2/ArrayInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class ArrayInput
+{
+    public static int[] ReadArrayFromConsole()
+    {
+        Console.Write("Enter the length of the array: ");
+        int arrayLength = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[arrayLength];
+
+        for (int elementIndex = 0; elementIndex < arrayLength; ++elementIndex)
+        {
+            Console.Write($"Enter element {elementIndex + 1}: ");
+            numbers[elementIndex] = int.Parse(Console.ReadLine());
+        }
+
+        return numbers;
+    }
+}
---
+++ Smooth sort.2/ArrayOutput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class ArrayOutput
+{
+    public static void PrintArray(int[] targetArray)
+    {
+        Console.WriteLine("Sorted array:");
+        for (int elementIndex = 0; elementIndex < targetArray.Length; ++elementIndex)
+        {
+            Console.Write(targetArray[elementIndex] + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
