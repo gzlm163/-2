a9d86c2b14c912d279221f8a252b0b2528c4f026
gzlm163 <marusyagzl@gmail.com>
2026-06-17 13:35:58 +0700

    singleton

---
+++ Smooth sort.1/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Singleton
+{
+    private static Singleton _instance;
+
+    private Singleton() { }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+
+    public void Method1()
+    {
+        Console.WriteLine("Singleton.Method1");
+    }
+
+    public void Method2()
+    {
+        Console.WriteLine("Singleton.Method2");
+    }
+}
